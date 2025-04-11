fe5e10ae1c0bdff373f52accf58cdbaa2152313f
A Dil <adilhasan2641@gmail.com>
2025-04-11 21:06:50 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Make local bin folder
+mkdir -p bin
+
+# Install ffmpeg (Render allows this)
 apt-get update && apt-get install -y ffmpeg
-curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
-chmod a+rx /usr/local/bin/yt-dlp
+
+# Download yt-dlp into local bin directory
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o bin/yt-dlp
+chmod a+rx bin/yt-dlp
