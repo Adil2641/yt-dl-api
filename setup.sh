9b380b78b8545ba9e5607be99de210a7480a14ab
A Dil <adilhasan2641@gmail.com>
2025-05-10 15:20:40 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,5 +7,5 @@ mkdir -p bin
 apt-get update && apt-get install -y ffmpeg
 
 # Download yt-dlp into bin folder
-curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o bin/yt-dlp
-chmod a+rx bin/yt-dlp
+wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O bin/yt-dlp
+chmod +x bin/yt-dl
